736510562ade4f3281ee48e8486fc0121ce31464
Dimitrios Kalemis <jimly1209@yahoo.gr>
2023-03-06 12:00:31 +0200

    rename main java class

--- src/main/java/com/example/minesweepr/Minesweeper.java
+++ src/main/java/com/example/minesweepr/Minesweeper.java
@@ -14,7 +14,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Random;
 
-public class HelloApplication extends Application {
+public class Minesweeper extends Application {
 
     public static Pane makeGrid(int n) {
         double width = 64;
@@ -127,7 +127,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(Minesweeper.class.getResource("hello-view.fxml"));
         //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         Scene scene = new Scene(makeGrid(10), 640, 640);
         stage.setTitle("Minesweeper!");
